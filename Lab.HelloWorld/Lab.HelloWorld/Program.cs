4cd30f0e0a14f27b830aa3289ae262da6bc0edc9
Matias Lane <matias.lane@outlook.com>
2021-09-07 11:21:04 -0300

    Creamos proyecto HelloWorld
    Subimos codigo del proyecto y solucion de ejercicio de HelloWorld.

---
+++ Lab.HelloWorld/Lab.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
